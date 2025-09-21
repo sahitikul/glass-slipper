0322ca937c7cb0f57199402da64907254943ffdf
Sahiti Kulkarni <sahitikul@Sahitis-MacBook-Air-2.local>
2025-09-21 09:09:18 -0400

    uploading pic frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+
+<html lang = "en">
+<head>
+    <meta charset = "UTF-8">
+    <meta name = "viewport"  content = "width=device-width, initial-scale=1.0">
+    <title> GlassSlipper.ai</title>
+</head>
+<body>
+    <h1>Welcome to GlassSlipper</h1>
+    <script src = "script.js"></script>
+</body>
+</html>
+
